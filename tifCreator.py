9f6244b92ebcb393fb813bf213a127d433e652a2
programmingprincess <jiaqiwu@asu.edu>
2019-04-20 00:57:31 -0500

    create tif records

---
+++ tifCreator.py
@@ -0,0 +1,4 @@
+import dataset_tool
+images_dir = "/Users/sushazad/Documents/Spring2019/CV/FFHQ/Sample/1"
+tif_dir = "/Users/sushazad/Documents/Spring2019/CV/FFHQ/Sample/TFFolder"
+dataset_tool.create_from_images(tif_dir, images_dir, 1)
